e81867657a61082d9a8262d047cd740a817c90f3
Satadru Bhowmik <2005687@kiit.ac.in>
2026-06-14 01:52:00 +0530

    feat: Add GuessInput widget for user input and submission handling

--- lib/main.dart
+++ lib/main.dart
@@ -75,9 +75,52 @@ class GamePage extends StatelessWidget {
               for (final letter in guess)
                 Tile(letter.char, letter.type),
             ],
-          )
+          ),
+        GuessInput(onSubmitGuess: (guess) {
+          print(guess);
+        })
         
       ]),
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (_) {
+                // print(_textEditingController.text);
+                onSubmitGuess(_textEditingController.text.trim());
+                _textEditingController.clear();
+                _focusNode.requestFocus();
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
